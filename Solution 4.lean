58c791ca9615c85d98168417ccb1a5c083eff273
Cam-Rice <RICEBC@email.sc.edu>
2023-04-27 11:08:54 -0400

    Push27

--- Solution 4.lean
+++ Solution 4.lean
@@ -16,44 +16,82 @@ def test : String :=
 
 .23456...  2-6
 ...45678.  4-8"
+-- only need to know values between spaces 11 and 15 (11,14]
 
 #eval test
 
 def cut (s : String) : List String := s.splitOn "\x0d\n"
 
-#eval cut test 
+def pair (s : String) : List Nat := 
+  let l' := (s.splitOn "\x0d\n")
+  l'.map (fun s => s.toNat!)
+
+#eval cut test
+#eval (cut test).map pair
 
 -- need to make a list of all the numbers between two inputs.
 -- attach each Nat.succ until last number
 -- where start of first list is or is greater than start or 2nd lists, record that.
 -- same for end of the list, but for the end. Must be both
 
-inductive Nats
-| zero : Nats
-| succ (n : Nat) : Nats
-def n : Nat := Nat.succ (Nat.succ Nat.zero)
-def add (m n : Nat) : Nat :=
-  match m with
-  | Nat.zero => n
-  | Nat.succ m' => Nat.succ (add m' n) 
-
-
-
 inductive number where
-  | One : 1
+  | one
+  | two
+  | three
+  | four
+  | five
+  | six
+  | seven
+  | eight
+  | nine
+  | Null 
+deriving Repr
 
+def inputs : String → number := by
+  intro a
+  match a with
+  | "1" => exact .one
+  | "2" => exact .two
+  | "3" => exact .three
+  | "4" => exact .four
+  | "5" => exact .five
+  | "6" => exact .six
+  | "7" => exact .seven
+  | "8" => exact .eight
+  | "9" => exact .nine
+  | _ => exact .Null
 
+def value (n : number) : Nat :=
+  match n with
+  | .one => 1
+  | .two => 2
+  | .three => 3
+  | .four => 4
+  | .five => 5
+  | .six => 6
+  | .seven => 7
+  | .eight => 8
+  | .nine => 9
+  | .Null => 0
 
+def divide (l : List a) : List (List a) :=
+match l with
+| [] => []
+| a::as =>
+match divide as with
+| [] => [[a]]
+| l::ls => if l.length = 2 then [a]::(l::ls) else (a::l)::ls
 
+#eval divide (cut test)
 
+--(List.tails l2).filter (fun l => l1.isPrefix l)
 
+def List.contSublist {α : Type} [BEq α] (l₁ l₂ : List α) : Bool :=                                              
+let b := l₂.tails.filter (fun l => l₁.isPrefixOf l)|>.isEmpty
+!b
 
-
-
-
-
-
-
+def List.eitherContSublist {α : Type} [BEq α] (l₁ l₂ : List α) : Bool :=
+l₁.contSublist l₂ || l₂.contSublist l₁
 
 example {α : Type} (a : α) (l: List α) : (a::l).length = 1 + l.length := by 
   match l with 
